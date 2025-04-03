10c05d795245e73056ae37bff19b0f2666af6339
Kunal Katarya <kunal.rox@gmail.com>
2025-04-03 08:05:40 +0800

    todo list with context

--- 10localStorage/src/App.jsx
+++ 10localStorage/src/App.jsx
@@ -23,12 +23,42 @@ function App() {
       prev.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
     );
   };
+
+  // use useEffect to query local storage, get data, use it to initialize the value of todos
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  // Update local storage when todos change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}
     >
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </TodoProvider>
   );
